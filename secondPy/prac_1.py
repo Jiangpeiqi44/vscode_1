557defa1a0d5da880f7cfb0e99218e7e291909e2
Jiangpeiqi44 <1360755367@qq.com>
2020-02-24 17:44:34 +0800

    prac_1_form_book

---
+++ secondPy/prac_1.py
@@ -0,0 +1,7 @@
+print("hello")
+message = " Hello_Python_interpreter! "
+print(message.rstrip())
+message.lstrip()
+message.strip()
+message=message.strip()
+print(message)
--- secondPy/test2.py
+++ secondPy/test2.py
@@ -1,1 +1,5 @@
 print("testGitHubfromPYC")
+for i in range(0,2):
+    print(i)
+    print(i+1)
+print(i) #incorrect in some IDE
