30ec34a49deac6f9c497b043a9813d493b7deed6
Jiangpeiqi44 <1360755367@qq.com>
2020-02-24 17:08:54 +0800

    testPy

--- test.py
+++ test.py
@@ -1,1 +1,6 @@
-print("Hello Python interpreter!")
+message = " Hello Python interpreter! "
+print(message.rstrip())
+print(message.lstrip())
+print(message.strip())
+message=message.strip()
+print(message)
---
+++ testFromG.py
@@ -0,0 +1,8 @@
+print("hello")
+message = " Hello_Python_interpreter! "
+print(message.rstrip())
+message.lstrip()
+message.strip()
+message=message.strip()
+print(message)
+
